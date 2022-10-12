36d3c74a342db9ac541b0096c76c54d89f11e1fb
Laura de Oliveira <l4ur4.88@gmail.com>
2022-10-12 13:44:26 -0300

    Add design system tokens in tailwind.config

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,6 @@ import './styles/global.css';
 
 export function App() {
   return (
-    <h1 className='text-5xl font-bold'>Hellow</h1>
+    <h1 className='text-sm text-black font-bold'>Hellow</h1>
   )
 }
